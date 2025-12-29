076893ca11a0624d4b1aa52acba1bc03c70de712
FlipArtYT <192363076+FlipArtYT@users.noreply.github.com>
2025-12-29 18:31:57 +0100

    Redesigned icons with awesome icons, add Quit menu, add page load progressbar and add setting to toggle JavaScript

--- browser/main.py
+++ browser/main.py
@@ -16,11 +16,14 @@ from PyQt6.QtWidgets import (
     QCheckBox,
     QDialog,
     QLabel,
-    QDialogButtonBox
+    QDialogButtonBox,
+    QProgressBar
 )
 from PyQt6.QtCore import Qt, QUrl
 from PyQt6.QtWebEngineWidgets import QWebEngineView
+from PyQt6.QtWebEngineCore import QWebEngineSettings
 from PyQt6.QtGui import QPixmap, QIcon, QAction
+import qtawesome as qta
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 CONFIG_PATH = os.path.join(SCRIPT_DIR, "config", "settings.json")
@@ -31,9 +34,11 @@ SEARCH_ENGINE_SEARCH_QUERIES = {
 }
 start_page = "https://silk-project.github.io/"
 search_engine = "Google"
+javascript_enabled = True
 default_settings = {
     "start_page_url":"https://silk-project.github.io/",
-    "search_engine":"Google"
+    "search_engine":"Google",
+    "javascript_enabled":True
 }
 
 # Load settings.json
@@ -41,10 +46,11 @@ if os.path.exists(CONFIG_PATH):
     with open(CONFIG_PATH, "r") as f:
         d = json.load(f)
 
-        if d["start_page_url"] and d["search_engine"]:
+        try:
             start_page = d["start_page_url"]
             search_engine = d["search_engine"]
-        else:
+            javascript_enabled = d["javascript_enabled"]
+        except KeyError:
             print("Failed to load settings.json.")
         
 else:
@@ -53,11 +59,13 @@ else:
         json.dump(default_settings, f, indent=4)
 
 class WebEngine():
-    def __init__(self, window, url_bar, prevbtn, nextbtn):
+    def __init__(self, window, url_bar, prevbtn, nextbtn, status_bar, page_progress):
         self.window = window
         self.url_bar = url_bar
         self.prevbtn = prevbtn
         self.nextbtn = nextbtn
+        self.status_bar = status_bar
+        self.page_progress = page_progress
 
         self.init_engine()
     
@@ -67,8 +75,9 @@ class WebEngine():
     
     def load_page(self, url):
         # Load URL if valid, else use the default search engine
-        if self.valid_url(url):
-            self.window.setUrl(QUrl(url))
+        processed_url = QUrl.fromUserInput(url).toString()
+        if self.valid_url(processed_url):
+            self.window.setUrl(QUrl(processed_url))
         else:
             # Get url for search engine
             search_url = SEARCH_ENGINE_SEARCH_QUERIES.get(search_engine) + url
@@ -76,6 +85,9 @@ class WebEngine():
         
         self.update_url_bar()
         self.update_nav_btn_status()
+    
+    def reload_page(self):
+        self.window.reload()
           
     def update_url_bar(self):
         url = self.window.url().toString()
@@ -86,6 +98,12 @@ class WebEngine():
         # Activate / Deactivate Back and Forward Buttons
         self.prevbtn.setEnabled(True if self.window.history().canGoBack() == True else False)
         self.nextbtn.setEnabled(True if self.window.history().canGoForward() == True else False)
+    
+    def page_load_finished(self):
+        self.page_progress.setValue(0)
+    
+    def update_page_progress(self, prog):
+        self.page_progress.setValue(prog)
     
     def valid_url(self, url):
         # Regex for standard http/https URLs
@@ -104,48 +122,30 @@ class WebEngine():
 
     def next_page(self):
         self.window.history().forward()
+    
+    def update_engine(self):
+        self.window.settings().setAttribute(QWebEngineSettings.WebAttribute.JavascriptEnabled, javascript_enabled)
 
 class BrowserWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
+        # Window configuration
         self.setWindowTitle("Silk Mizu")
         self.setMinimumSize(960, 720)
-        self.init_ui()
+        self.layout = QVBoxLayout()
 
-    def init_ui(self):
-        layout = QVBoxLayout()
-        controls_layout = QHBoxLayout()
-        layout.addLayout(controls_layout)
+        # Initialize whole UI
+        self.init_menu_status_bar()
+        self.init_control_ui()
+        self.init_web_engine()
 
-        # Browser main controls
-        self.prev_page_btn = QPushButton("<")
-        self.prev_page_btn.setStyleSheet("padding: 10px;")
-        self.prev_page_btn.clicked.connect(self.request_back_page)
-        controls_layout.addWidget(self.prev_page_btn)
-
-        self.next_page_btn = QPushButton(">")
-        self.next_page_btn.setStyleSheet("padding: 10px;")
-        self.next_page_btn.clicked.connect(self.request_next_page)
-        controls_layout.addWidget(self.next_page_btn)
-
-        self.url_bar = QLineEdit()
-        self.url_bar.setStyleSheet("padding: 10px;")
-        self.url_bar.clearFocus()
-        self.url_bar.returnPressed.connect(self.request_load_page)
-        controls_layout.addWidget(self.url_bar)
-
-        self.load_btn = QPushButton("Load")
-        self.load_btn.setStyleSheet("padding: 10px;")
-        self.load_btn.clicked.connect(self.request_load_page)
-        controls_layout.addWidget(self.load_btn)
-
-        # Web Engine
-        self.web_widget = QWebEngineView()
-        self.web_engine = WebEngine(self.web_widget, self.url_bar, self.prev_page_btn, self.next_page_btn)
-        self.web_widget.urlChanged.connect(self.web_engine.update_url_bar)
-        layout.addWidget(self.web_widget)
-
+        # Add main widget
+        widget = QWidget()
+        widget.setLayout(self.layout)
+        self.setCentralWidget(widget)
+    
+    def init_menu_status_bar(self):
         # Add menu bar
         menu_bar = self.menuBar()
 
@@ -155,7 +155,12 @@ class BrowserWindow(QMainWindow):
         helpMenu = menu_bar.addMenu("&Help")
 
         # File Menu
+        exitAction = fileMenu.addAction("Quit")
+        exitAction.triggered.connect(sys.exit)
+        fileMenu.addAction(exitAction)
+
         # Edit Menu
+
         # Settings Menu
         settingsAction = settingsMenu.addAction("Program Settings")
         settingsAction.triggered.connect(self.settings_dialog)
@@ -170,14 +175,80 @@ class BrowserWindow(QMainWindow):
         aboutAction.triggered.connect(self.about_dialog)
         helpMenu.addAction(aboutAction)
 
-        # Add main widget
-        widget = QWidget()
-        widget.setLayout(layout)
-        self.setCentralWidget(widget)
+        # Add status bar
+        self.status_bar = self.statusBar()
+        self.page_progressbar = QProgressBar()
+        self.page_progressbar.setFixedWidth(200)
+        self.page_progressbar.setValue(0)
+
+        self.status_bar.addWidget(self.page_progressbar)
+
+    def init_control_ui(self):
+        # Add main controls
+        controls_layout = QHBoxLayout()
+        self.layout.addLayout(controls_layout)
+
+        # Browser main controls
+        self.prev_page_btn = QPushButton()
+        self.prev_page_btn.setIcon(qta.icon("fa6s.arrow-left"))
+        self.prev_page_btn.setStyleSheet("padding: 10px;")
+        self.prev_page_btn.clicked.connect(self.request_back_page)
+        controls_layout.addWidget(self.prev_page_btn)
+
+        self.next_page_btn = QPushButton()
+        self.next_page_btn.setIcon(qta.icon("fa6s.arrow-right"))
+        self.next_page_btn.setStyleSheet("padding: 10px;")
+        self.next_page_btn.clicked.connect(self.request_next_page)
+        controls_layout.addWidget(self.next_page_btn)
+
+        self.reload_page_btn = QPushButton()
+        self.reload_page_btn.setIcon(qta.icon("fa6s.arrow-rotate-right"))
+        self.reload_page_btn.setStyleSheet("padding: 10px;")
+        self.reload_page_btn.clicked.connect(self.request_reload_stop_page)
+        controls_layout.addWidget(self.reload_page_btn)
+
+        self.url_bar = QLineEdit()
+        self.url_bar.setStyleSheet("padding: 10px;")
+        self.url_bar.clearFocus()
+        self.url_bar.returnPressed.connect(self.request_load_page)
+        controls_layout.addWidget(self.url_bar)
+
+        self.load_btn = QPushButton("Go")
+        self.load_btn.setIcon(qta.icon("mdi.arrow-right-bold-box"))
+        self.load_btn.setStyleSheet("padding: 10px;")
+        self.load_btn.clicked.connect(self.request_load_page)
+        controls_layout.addWidget(self.load_btn)
+
+        self.settings_btn = QPushButton()
+        self.settings_btn.setIcon(qta.icon("fa5s.cog"))
+        self.settings_btn.setStyleSheet("padding: 10px;")
+        self.settings_btn.clicked.connect(self.settings_dialog)
+        controls_layout.addWidget(self.settings_btn)
+    
+    def init_web_engine(self):
+        # Web Engine
+        self.web_widget = QWebEngineView()
+        self.web_engine = WebEngine(self.web_widget,
+                                    self.url_bar,
+                                    self.prev_page_btn,
+                                    self.next_page_btn,
+                                    self.status_bar,
+                                    self.page_progressbar)
+        self.web_widget.urlChanged.connect(self.web_engine.update_url_bar)
+        self.web_widget.loadProgress.connect(self.web_engine.update_page_progress)
+        self.web_widget.loadFinished.connect(self.web_engine.page_load_finished)
+        self.layout.addWidget(self.web_widget)
 
     def request_load_page(self):
         url = self.url_bar.text()
         self.web_engine.load_page(url)
+    
+    def request_reload_stop_page(self):
+        if True:
+            self.web_engine.reload_page()
+        else:
+            # ...
+            self.web_engine.stop_page()
     
     def request_back_page(self):
         self.web_engine.back_page()
@@ -186,6 +257,10 @@ class BrowserWindow(QMainWindow):
         self.web_engine.next_page()
     
     def settings_dialog(self):
+        global start_page
+        global search_engine
+        global javascript_enabled
+
         dlg = QDialog(self)
         dlg.setWindowTitle("Settings")
         dlg.setFixedSize(480, 360)
@@ -201,12 +276,16 @@ class BrowserWindow(QMainWindow):
         start_page_lineedit = QLineEdit()
         start_page_lineedit.setText(start_page)
         start_page_lineedit.setMinimumWidth(200)
-        settings_layout.addRow("Start Page: ", start_page_lineedit)
+        settings_layout.addRow("Start page: ", start_page_lineedit)
 
         search_engine_combobox = QComboBox()
         search_engine_combobox.addItems(["Google", "DuckDuckGo"])
         search_engine_combobox.setCurrentText(search_engine)
-        settings_layout.addRow("Search Engine: ", search_engine_combobox)
+        settings_layout.addRow("Search engine: ", search_engine_combobox)
+
+        javascript_checkbox = QCheckBox()
+        javascript_checkbox.setChecked(javascript_enabled)
+        settings_layout.addRow("Javascript enabled", javascript_checkbox)
 
         button_box = QDialogButtonBox(QDialogButtonBox.StandardButton.Ok | QDialogButtonBox.StandardButton.Cancel)
         button_box.accepted.connect(dlg.accept)
@@ -218,13 +297,23 @@ class BrowserWindow(QMainWindow):
         dlg.setLayout(layout)
 
         if dlg.exec():
+            start_page = start_page_lineedit.text()
+            search_engine = search_engine_combobox.currentText()
+            javascript_enabled = javascript_checkbox.isChecked()
+
             settings = {
-                "start_page_url":start_page_lineedit.text(),
-                "search_engine":search_engine_combobox.currentText()
+                "start_page_url":start_page,
+                "search_engine":search_engine,
+                "javascript_enabled":javascript_enabled
             }
+
+            self.update_web_engine()
 
             with open(CONFIG_PATH, "w") as f:
                 json.dump(settings, f, indent=4)
+    
+    def update_web_engine(self):
+        self.web_engine.update_engine()
         
     def about_dialog(self):
         dlg = QDialog(self)
